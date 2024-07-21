53a09a2614e65fad56699390801070ccd0e77507
Raphael Sotto-Maior <raphael.rodrigues@estudante.ufjf.br>
2024-07-21 10:27:31 -0300

    .

---
+++ OtimizadorDeCompraOnline/src/main/java/com/mycompany/otimizadordecompraonline/Cupom.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.otimizadordecompraonline;
+
+/**
+ *
+ * @author Raphael Sotto-Maior
+ */
+public class Cupom {
+    
+    String tipo;
+    String categoria;
+    float desconto;
+}
