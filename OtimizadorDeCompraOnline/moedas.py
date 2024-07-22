6351d6ebe5576572de3f2f61b0f8b96c43bf2908
Raphael Rodrigues Sotto-Maior <raphael.rodrigues@estudante.ufjf.br>
2024-07-22 14:38:48 -0300

    Classe "Moeda" e gerador de moedas para teste.

---
+++ OtimizadorDeCompraOnline/moedas.py
@@ -0,0 +1,25 @@
+import random
+from datetime import datetime, timedelta
+
+# Função para gerar uma data aleatória entre inicio e fim
+def data_aleatoria(inicio, fim):
+    delta = fim - inicio
+    random_days = random.randint(0, delta.days)
+    return inicio + timedelta(days=random_days)
+
+# Defina o intervalo de tempo
+inicio = datetime.strptime('2020-01-01', '%Y-%m-%d')
+fim = datetime.now()
+
+# Gere uma lista de datas aleatórias
+num_datas = random.randint(10,100)  # Número de datas a serem geradas
+print(num_datas)
+random_dates = [data_aleatoria(inicio, fim).strftime('%Y-%m-%d') for _ in range(num_datas)]
+random_dates.sort()
+
+# Salve a lista de datas em um arquivo txt
+with open('listamoedas.txt', 'w') as file:
+    for date in random_dates:
+        file.write(date + '\n')
+
+print("Lista de datas aleatórias gerada e salva em random_dates.txt.")
